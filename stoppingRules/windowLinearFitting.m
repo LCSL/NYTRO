d503580c244db4d317d54078dc8490e7e29aaa8c
Raffaello Camoriano <raffaello.camoriano@iit.it>
2015-10-21 15:12:12 +0200

    stopping rule cleanup

--- stoppingRules/windowLinearFitting.m
+++ stoppingRules/windowLinearFitting.m
@@ -14,20 +14,6 @@ function stop = windowLinearFitting(err , winSize , thres)
         X = [ones(winSize,1), x];
         b = X\currErr';
         
-        figure(1)
-        plot(x,currErr);
-        hold on;
-        plot(x,b(2)*x + b(1));
-        legend('Error curve' , 'Linear fitting')
-        hold off
-        drawnow
-        
-%         if b(2) >= -(thres/winSize)
-%             stop = 1;
-%         end
-%         if b(2) >= atan(thres)
-%             stop = 1;
-%         end
         if (l*b(2)/b(1)) >= thres
             stop = 1;
         end
